062924f9f3d636a22f84fc4d6cbbedc4db9a6cb4
vatsal2312 <83644350+vatsal2312@users.noreply.github.com>
2021-11-19 16:48:41 +0530

    Update IStakingRewards.sol

--- interfaces/IStakingRewards.sol
+++ interfaces/IStakingRewards.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.0;
 
-import "../sv/IERC20.sol";
+import "/home/sv/MoreMoneyFinance_New/sv/IERC20.sol";
 
 interface IStakingRewards {
     function rewardsToken() external view returns (IERC20);
